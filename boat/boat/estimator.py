956d4aff54376459d2e02104bd6c405fd0005e55
Théo Magne <theo.magne.fr@gmail.com>
2023-11-25 21:43:08 +0100

    add theta and speed to the baotState

--- boat/boat/estimator.py
+++ boat/boat/estimator.py
@@ -2,10 +2,12 @@ import rclpy
 import math
 from rclpy.node import Node
 from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import Imu
 from environment_interfaces.msg import BoatState
 
 # Topics used by the estimator
 in_topic_gps = '/wamv/sensors/gps/gps/fix'
+in_topic_imu = '/wamv/sensors/imu/imu/data'
 
 # Topics published by the estimator
 out_topic_pos = '/boat/estimator/position'
@@ -15,6 +17,13 @@ class Estimator(Node):
         super().__init__('estimator')
         self.x = 0
         self.y = 0
+        self.theta = 0
+        self.previous_x = 0
+        self.previous_y = 0
+        self.vx = 0
+        self.vy = 0
+        self.previous_time_s = 0
+        self.previous_time_ns = 0
         self.base_lat = None
         self.base_lon = None
 
@@ -25,25 +34,59 @@ class Estimator(Node):
             self.gps_callback,
             10)
         self.subscription_gps  # prevent unused variable warning
+        self.subscription_imu = self.create_subscription(
+            Imu,
+            in_topic_imu,
+            self.imu_callback,
+            10)
+        self.subscription_imu # prevent unused variable warning
+
         # Pubs
         self.publisher_pos = self.create_publisher(BoatState, out_topic_pos, 10)
 
-    def gps_callback(self, msg):
+    def gps_callback(self, msg: NavSatFix):
         latitude = msg.latitude
         longitude = msg.longitude
-
+        # On récupère la position en x et y
         if self.base_lat is None and self.base_lon is None:
             self.base_lat, self.base_lon = latitude, longitude
             self.x, self.y = 0, 0
         else:
             self.x, self.y = self.convert_gps_to_xy(latitude, longitude)
-        # Affichage de la position en x et y avec 3 décimakes
-        self.get_logger().info('x: %.3f, y: %.3f' % (self.x, self.y))
-        # Publication de la position en x et y
-        msg = BoatState()
+        # On calcule la vitesse en x et y
+        sec = msg.header.stamp.sec
+        nsec = msg.header.stamp.nanosec
+        dt = (sec - self.previous_time_s) + ((nsec - self.previous_time_ns) / 1e9)
+        self.vx = (self.x - self.previous_x) / dt
+        self.vy = (self.y - self.previous_y) / dt
+        self.previous_time_s = sec
+        self.previous_time_ns = nsec
+        self.previous_x = self.x
+        self.previous_y = self.y
+        # Publish the state of the boat
+        self.publish_state()
+
+    def imu_callback(self, msg):
+        # Get the orientation of the boat from the IMU
+        x = msg.orientation.x
+        y = msg.orientation.y
+        z = msg.orientation.z
+        w = msg.orientation.w
+        self.theta = math.atan2(2 * (w * z + x * y), 1 - 2 * (y * y + z * z))
+        # Publish the state of the boat
+        self.publish_state()
+
+    def publish_state(self):
+        # Publish the state of the boat
+        msg: BoatState = BoatState()
         msg.x = float(self.x)
         msg.y = float(self.y)
+        msg.vx = float(self.vx)
+        msg.vy = float(self.vy)
+        msg.theta = float(self.theta)
         self.publisher_pos.publish(msg)
+
+
 
     def convert_gps_to_xy(self, latitude, longitude):
         # Haversine distance calculation
