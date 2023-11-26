14094f1f4396bb46001ed6a3f3d2017ffb66454e
ecoulon <clement.guais@reseau.eseo.fr>
2023-11-26 16:07:26 +0100

    refactor

--- planificator_traj/src/planificator_node.cpp
+++ planificator_traj/src/planificator_node.cpp
@@ -2,179 +2,51 @@
 #include <vector>
 #include <cmath>
 #include "rclcpp/rclcpp.hpp"
+#include "environment_interfaces/msg/boat_trajectory.hpp"
+
 #include "geometry_msgs/msg/point.hpp"
 #include "std_msgs/msg/string.hpp"
 
+class TrajectoryPlanner : public rclcpp::Node {
+private :
+    const int DELTA = 2;
 
-const int DELTA = 2;
-
-struct Point {
+    struct Point {
     int x, y;
 
     Point(int x, int y) : x(x), y(y) {}
 
+
     double distanceTo(const Point &other) {
         return std::hypot(double(x - other.x), double(y - other.y));
     }
-};
 
-struct Obstacle {
-    Point position;
-    int diametre;
-
-    Obstacle(const Point &pos, int diam) : position(pos), diametre(diam) {}
-};
-
-struct Node {
-    Point position;
-    Point velocity;
-    double restant_score;
-
-    Node(const Point &pos, double f) : position(pos), restant_score(f), velocity(*new Point(0, 0)) {}
-
-    bool operator>(const Node &other) const {
-        return restant_score > other.restant_score;
+    bool operator!=(const Point &other) const {
+        return (x != other.x && y != other.y);
     }
 };
 
-Point gps_to_Point(double latitude, double longitude) {
-    double lat_rad = M_PI / 180.0 * latitude;
-    double lon_rad = M_PI / 180.0 * longitude;
+    struct Obstacle {
+        Point position;
+        int diametre;
 
-    double R = 6371000.0;
+        Obstacle(const Point &pos, int diam) : position(pos), diametre(diam) {}
+    };
 
-    double x = R * lon_rad;
-    double y = R * log(tan(M_PI/4 + lat_rad/2));
+    struct NodeTraj {
+        Point position;
+        double restant_score;
+        Point velocity;
 
-    return *new Point(x, y);
-}
+        NodeTraj(Point& pos, double f) : position(pos), restant_score(f), velocity(*new Point(0, 0)) {}
 
-
-bool isValidPoint(Point &point, const std::vector<Obstacle *> &obstacles) {
-    for (auto obs : obstacles) {
-        if (point.x >= (obs->position.x - obs->diametre) 
-            && point.x <= (obs->position.x + obs->diametre) 
-            && point.y >= (obs->position.y - obs->diametre) 
-            && point.y <= (obs->position.y + obs->diametre)) {
-            return false;
-        }    
-    }
-    return true;
-}
-
-bool isInPath(const Point &neighbor, std::vector<Node *> path) {
-    for (auto node : path) {
-        if (neighbor.x == node->position.x && neighbor.y == node->position.y) {
-            return true;
+        bool operator>(const NodeTraj &other) const {
+            return restant_score > other.restant_score;
         }
-    }
-    return false;
-}
-
-std::vector<Point> planificationTrajectoire(Point &start, Point &initial_velocity, const Point &goal,
-                                            std::vector<Obstacle *> &obstacles, std::vector<Obstacle *> &obstaclesBateau) {
-    std::vector<Node*> path;
-    std::vector<Point> path_point;
-    bool traj_found = false;
-    bool is_depart = true;
-    int i = 0;
-
-    Node* initial_node = new Node(start, start.distanceTo(goal));
-    initial_node->velocity = initial_velocity;
-    path.push_back(initial_node);
-
-    while (!traj_found && i < 2000) {
-
-        if (i >= 5) {
-            is_depart = false;
-        }
-
-        Node* current = path.back();
-
-        if (current->position.distanceTo(goal) <= 26) {
-            // Objectif atteint, reconstruire la trajectoire
-            while (!path.empty()) {
-                path_point.push_back(path.back()->position);
-                path.pop_back();
-            }
-            std::reverse(path_point.begin(), path_point.end());
-            return path_point;
-        }
-
-        Point best_neighbor = Point(-1000, -1000);
-
-        for (const Point& neighbor : {
-                // Calculez les positions des voisins en fonction de la vitesse initiale
-                Point(current->position.x + DELTA + current->velocity.x, current->position.y + current->velocity.y),
-                Point(current->position.x - DELTA + current->velocity.x, current->position.y + current->velocity.y),
-                Point(current->position.x + current->velocity.x, current->position.y + DELTA + current->velocity.y),
-                Point(current->position.x + current->velocity.x, current->position.y - DELTA + current->velocity.y),
-                Point(current->position.x + DELTA + current->velocity.x, current->position.y + DELTA + current->velocity.y),
-                Point(current->position.x + DELTA + current->velocity.x, current->position.y - DELTA + current->velocity.y),
-                Point(current->position.x - DELTA + current->velocity.x, current->position.y + DELTA + current->velocity.y),
-                Point(current->position.x - DELTA + current->velocity.x, current->position.y - DELTA + current->velocity.y)
-            }) {
-            Point mutable_neighbor = neighbor;
-            if (isValidPoint(mutable_neighbor, obstacles) && isValidPoint(mutable_neighbor, obstaclesBateau)&& !isInPath(neighbor, path)) {  
-                
-                double restant_score_tentative = mutable_neighbor.distanceTo(goal);
-
-                
-                    if (path.size() >= 2) {
-
-                        if (current->position.distanceTo(neighbor) < path.at(path.size()-2)->position.distanceTo(neighbor)) {
-                            if (best_neighbor.x == -1000 && best_neighbor.y == -1000) {
-                                best_neighbor = neighbor;
-                            }
-                            else if (restant_score_tentative < best_neighbor.distanceTo(goal)) {
-                                best_neighbor = neighbor;
-                            }
-                        }
-                    }
-                    else {
-                        if (best_neighbor.x == -1000 && best_neighbor.y == -1000) {
-                            best_neighbor = neighbor;
-                        }
-                        else if (restant_score_tentative < best_neighbor.distanceTo(goal)) {
-                            best_neighbor = neighbor;
-                        }
-                    }
-                    
-                
-            }
-        }
-        if (best_neighbor.x == 90 && best_neighbor.y == -50) {
-            std::cout << "putain de merde ";
-        }
-        path.push_back(new Node(best_neighbor, best_neighbor.distanceTo(goal)));
-        i++;
-    }
-    return path_point;
-}
-
-std::vector<Point> minimizePath(std::vector<Point> &path) {
-    std::vector<Point> path_point;
-    path_point.push_back(path.at(0));
-
-    for (int i = 0; i < (int)path.size()-3; i++ ) {
-        Point first_point = path.at(i);
-        Point second_point = path.at(i+1);
-        Point third_point = path.at(i+2);
-
-        if ((second_point.x - first_point.x) != (third_point.x - second_point.x) || (second_point.y - first_point.y) != (third_point.y - second_point.y)) {
-            path_point.push_back(second_point);
-        }
-
-    }
-
-    return path_point;
-}
-
-class TrajectoryPlanner : public rclcpp::Node {
-private :
+    };
 
     // Configuration
-    bool isObstacles = false;
+    bool isObstacles = true;
     std::vector<Obstacle *> obstacles = {new Obstacle(*new Point(120, -50), 25),
                                         new Obstacle(*new Point(-152, -6), 50),
                                         new Obstacle(*new Point(110, 130), 50),
@@ -185,6 +57,7 @@ private :
                                         new Obstacle(*new Point(-44, -95), 30),
                                         new Obstacle(*new Point(-30, -150), 30)};
     bool isPursuit = true;
+    bool isFirstTrajPat = true;
 
     // Infos Bateau
     Point start = Point(-1000,-1000);
@@ -193,23 +66,22 @@ private :
     std::vector<Obstacle *> obstaclesBateau = {};
 
     //Topics
-    // rclcpp::Subscription<Point*>::SharedPtr start_subscription_;
-    // rclcpp::Subscription<Point*>::SharedPtr goal_subscription_;
-    // rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr traj_publisher_;
+    rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr start_subscription_;
+    rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr goal_subscription_;
+    rclcpp::Publisher<environment_interfaces::msg::BoatTrajectory>::SharedPtr traj_publisher_;
 
-    // void startCallback(const Point* msg) {
+    void startCallback(geometry_msgs::msg::Point msg) {
 
-    //     Point tmpPoint = gps_to_Point(msg->x,msg->y);
-    //     this.start.x = tmpPoint.x + this.initialPoint.x;
-    //     this.start.y = tmpPoint.y + this.initialPoint.y;
-    //     calculateAndPublishTrajectory();
-    // }
+        this->start.x = msg.x;
+        this->start.y = msg.y;
+        // calculateAndPublishTrajectory();
+    }
 
-    // void goalCallback(const Point* msg) {
-    //     Point tmpPoint = gps_to_Point(msg->x,msg->y);
-    //     this.goal.x = tmpPoint.x + this.initialPoint.x;
-    //     this.goal.y = tmpPoint.y + this.initialPoint.y;
-    // }
+    void goalCallback(geometry_msgs::msg::Point msg) {
+
+        this->goal.x = msg.x;
+        this->goal.y = msg.y;
+    }
 
     // void obstaclesCallback(const std::vector<Point*> msg) {
     //     std::vector<Obstacle *> l_obstaclesBateau = {};
@@ -220,36 +92,163 @@ private :
     //     this.obstaclesBateau = l_obstaclesBateau;
     // }
 
-    // void calculateAndPublishTrajectory() {
+    void calculateAndPublishTrajectory() {
 
-    //     if(!isPursuit){
-    //         // Algo pour goal patrouille
-    //     }
+        if(this->start != Point(-1000,-1000) && this->goal != Point(-1000,-1000)){
+            if(!isPursuit){
+                if(isFirstTrajPat){
+                    this->goal = Point(200,-200);
+                }else{
+                    if(this->start.x > 150 && this->start.y < -150){
+                        this->goal = Point(-200,-200);
+                    }else if(this->start.x < -150 && this->start.y < -150){
+                        this->goal = Point(-200,200);
+                    }else if(this->start.x < -150 && this->start.y > 150){
+                        this->goal = Point(0,0);
+                    }else if(this->start.x > -100 && this->start.x < 100 && this->start.y > -100 && this->start.y < 100){
+                        this->goal = Point(200,200);
+                    }else if(this->start.x > 150 && this->start.y > 150){
+                        this->goal = Point(200,-200);
+                    } 
+                }
+            }
 
-    //     std::vector<Point> path = planificationTrajectoire(this.start, this.initialVelocity, this.goal, this.obstacles, this.obstaclesBateau);
-    //     std::vector<Point> min_path = minimizePath(path);
+            std::vector<Point> path = planificationTrajectoire(this->start, this->initialVelocity, this->goal, this->obstacles, this->obstaclesBateau);
+            std::vector<Point> min_path = minimizePath(path);
 
-    //     // Publish the trajectory
-    //     custom_msg::msg::Trajectory traj_msg;
-    //     for (const auto &point : min_path) {
-    //         traj_msg.x_points.push_back(point.x);
-    //         traj_msg.y_points.push_back(point.y);
-    //     }
+            // Publish the trajectory
+            environment_interfaces::msg::BoatTrajectory traj_msg;
+            for (int i = 0; i<4;i++) {
+                traj_msg.x[i] = min_path.at(i).x;
+                traj_msg.y[i] = min_path.at(i).y;
+            }
 
-    //     traj_publisher_->publish(traj_msg);
-    // }
+            traj_publisher_->publish(traj_msg);
+        }   
+    }
 
 public:
     TrajectoryPlanner()
         : Node("trajectory_planner_node") {
 
-            // this->start_subscription_ = this->create_subscription<geometry_msgs::msg::Point>(
-            //     "/start", 10, std::bind(&TrajectoryPlanner::startCallback, this, std::placeholders::_1));
+            this->start_subscription_ = this->create_subscription<geometry_msgs::msg::Point>(
+                "/start", 10, std::bind(&TrajectoryPlanner::startCallback, this, std::placeholders::_1));
 
-            // this->goal_subscription_ = this->create_subscription<geometry_msgs::msg::Point>(
-            //     "/goal", 10, std::bind(&TrajectoryPlanner::goalCallback, this, std::placeholders::_1));
+            this->goal_subscription_ = this->create_subscription<geometry_msgs::msg::Point>(
+                "/goal", 10, std::bind(&TrajectoryPlanner::goalCallback, this, std::placeholders::_1));
 
-            // this->traj_publisher_ = this->create_publisher<geometry_msgs::msg::Point>("/traj", 10);
+            this->traj_publisher_ = this->create_publisher<environment_interfaces::msg::BoatTrajectory>("/traj", 10);
+    }
+
+    std::vector<Point> planificationTrajectoire(Point &start, Point &initial_velocity, const Point &goal,
+                                            std::vector<Obstacle *> &obstacles, std::vector<Obstacle *> &obstaclesBateau) {
+        std::vector<NodeTraj*> path;
+        std::vector<Point> path_point;
+        bool traj_found = false;
+        int i = 0;
+
+        NodeTraj* initial_node = new NodeTraj(start, start.distanceTo(goal));
+        initial_node->velocity = initial_velocity;
+        path.push_back(initial_node);
+
+        while (!traj_found && i < 2000) {
+
+            NodeTraj* current = path.back();
+
+            if (current->position.distanceTo(goal) <= 26) {
+                // Objectif atteint, reconstruire la trajectoire
+                while (!path.empty()) {
+                    path_point.push_back(path.back()->position);
+                    path.pop_back();
+                }
+                std::reverse(path_point.begin(), path_point.end());
+                return path_point;
+            }
+
+            Point best_neighbor = Point(-1000, -1000);
+
+            for (const Point& neighbor : {
+                    // Calculez les positions des voisins en fonction de la vitesse initiale
+                    Point(current->position.x + DELTA + current->velocity.x, current->position.y + current->velocity.y),
+                    Point(current->position.x - DELTA + current->velocity.x, current->position.y + current->velocity.y),
+                    Point(current->position.x + current->velocity.x, current->position.y + DELTA + current->velocity.y),
+                    Point(current->position.x + current->velocity.x, current->position.y - DELTA + current->velocity.y),
+                    Point(current->position.x + DELTA + current->velocity.x, current->position.y + DELTA + current->velocity.y),
+                    Point(current->position.x + DELTA + current->velocity.x, current->position.y - DELTA + current->velocity.y),
+                    Point(current->position.x - DELTA + current->velocity.x, current->position.y + DELTA + current->velocity.y),
+                    Point(current->position.x - DELTA + current->velocity.x, current->position.y - DELTA + current->velocity.y)
+                }) {
+                Point mutable_neighbor = neighbor;
+                if (isValidPoint(mutable_neighbor, obstacles) && isValidPoint(mutable_neighbor, obstaclesBateau)&& !isInPath(neighbor, path)) {  
+                    
+                    double restant_score_tentative = mutable_neighbor.distanceTo(goal);
+
+                    
+                        if (path.size() >= 2) {
+
+                            if (current->position.distanceTo(neighbor) < path.at(path.size()-2)->position.distanceTo(neighbor)) {
+                                if (best_neighbor.x == -1000 && best_neighbor.y == -1000) {
+                                    best_neighbor = neighbor;
+                                }
+                                else if (restant_score_tentative < best_neighbor.distanceTo(goal)) {
+                                    best_neighbor = neighbor;
+                                }
+                            }
+                        }
+                        else {
+                            if (best_neighbor.x == -1000 && best_neighbor.y == -1000) {
+                                best_neighbor = neighbor;
+                            }
+                            else if (restant_score_tentative < best_neighbor.distanceTo(goal)) {
+                                best_neighbor = neighbor;
+                            }
+                        }
+                        
+                    
+                }
+            }
+            path.push_back(new NodeTraj(best_neighbor, best_neighbor.distanceTo(goal)));
+            i++;
+        }
+        return path_point;
+    }
+
+    bool isValidPoint(Point &point, const std::vector<Obstacle *> &obstacles) {
+        for (auto obs : obstacles) {
+            if (point.x >= (obs->position.x - obs->diametre) 
+                && point.x <= (obs->position.x + obs->diametre) 
+                && point.y >= (obs->position.y - obs->diametre) 
+                && point.y <= (obs->position.y + obs->diametre)) {
+                return false;
+            }    
+        }
+        return true;
+    }
+
+    bool isInPath(const Point &neighbor, std::vector<NodeTraj *> path) {
+        for (auto node : path) {
+            if (neighbor.x == node->position.x && neighbor.y == node->position.y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    std::vector<Point> minimizePath(std::vector<Point> &path) {
+        std::vector<Point> path_point;
+
+        for (int i = 0; i < (int)path.size()-3; i++ ) {
+            Point first_point = path.at(i);
+            Point second_point = path.at(i+1);
+            Point third_point = path.at(i+2);
+
+            if ((second_point.x - first_point.x) != (third_point.x - second_point.x) || (second_point.y - first_point.y) != (third_point.y - second_point.y)) {
+                path_point.push_back(second_point);
+            }
+
+        }
+
+        return path_point;
     }
 };
 
