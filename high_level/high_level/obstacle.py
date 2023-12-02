20bbdd48ce804f11ba876676e72cdfff9c698469
Eliot Coulon <eliot.coulon@reseau.eseo.fr>
2023-12-02 21:29:16 +0100

    Update obstacles.py

--- high_level/high_level/obstacle.py
+++ high_level/high_level/obstacle.py
@@ -2,11 +2,16 @@ KEEPOUT_RADIUS = 10
 
 def get_obstacles():
     obstacles = [
+        [-157, 0, 35 + KEEPOUT_RADIUS],
+        [-147, -20, 35 + KEEPOUT_RADIUS],
+
+        [120, 130, 35 + KEEPOUT_RADIUS],
+        [90, 150, 35 + KEEPOUT_RADIUS],
+        [100, 170, 35 + KEEPOUT_RADIUS],
+        [110, 150, 30 + KEEPOUT_RADIUS],
+        
         [120, -50, 25 + KEEPOUT_RADIUS],
-        [-152, -6, 50 + KEEPOUT_RADIUS],
-        [110, 130, 50 + KEEPOUT_RADIUS],
         [12, -102, 25 + KEEPOUT_RADIUS],
-        [92, 170, 25 + KEEPOUT_RADIUS],
         [-92, 176, 30 + KEEPOUT_RADIUS],
         [-40, 220, 30 + KEEPOUT_RADIUS],
         [-44, -95, 30 + KEEPOUT_RADIUS],
